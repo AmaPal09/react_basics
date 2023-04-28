02da6af3d152c38cf85edfcbfae76bca6fe1e070
Amarja Palekar <amarjapalekar09@gmail.com>
2023-04-28 15:37:02 -0700

    Add key to all list items to be rendered
    Add unique keys to all list items that are to be rendered

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const Person = props => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, props.name),
-		React.createElement('p', {}, props.occupation)
+	return React.createElement('div', {key:'a1'}, [
+		React.createElement('h1', {key: props.id}, props.name),
+		React.createElement('p', {key: props.name}, props.occupation)
 		])
 
 };
 
 const App = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {class: 'title'}, ["React is rendered"]),
-		React.createElement(Person, {name: 'Ama', occupation: 'Developer'}, null),
-		React.createElement(Person, {name: 'Ama2', occupation: 'Dev2'}, null),
-		React.createElement(Person, {name: 'Ama3', occupation: 'Dev2'}, null)
+	return React.createElement('div', {key: 0}, [
+		React.createElement('h1', {className: 'title', key: 99}, ["React is rendered"]),
+		React.createElement(Person, {name: 'Ama', occupation: 'Developer', id: 1, key: 1}, null),
+		React.createElement(Person, {name: 'Ama2', occupation: 'Dev2', id: 2,  key: 2}, null),
+		React.createElement(Person, {name: 'Ama3', occupation: 'Dev2', id: 3, key: 3}, null)
 		]);
 };
 
